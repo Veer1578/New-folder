dfd61932bbd1f106792fb86eb53b6e12a2fab726
Veer1578 <sweetyatulshah@gmail.com>
2025-06-26 15:29:14 +0530

    event_handler

---
+++ Event handler.py
@@ -0,0 +1,25 @@
+from tkinter import *
+
+root = Tk()
+root.title('Event Handler')
+root.geometry('100x100')
+
+def key_pressed(event):
+    '''Print the character associated to the key press'''
+    print(event.char)
+
+root.bind('<Key>', key_pressed)
+
+
+def button_pressed(event):
+    '''Print the character associated to the key press'''
+    print('The button was clicked')
+
+buttn = Button(text='Click Me')
+buttn.pack()
+
+
+root.bind('<Button-1>', button_pressed)
+
+root.mainloop()
+
